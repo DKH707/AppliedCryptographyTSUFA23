e824a32ac5b1d66113ec77a188f7fae4b53a475e
DKH707 <102496057+DKH707@users.noreply.github.com>
2023-11-29 11:51:03 -0600

    Added Encryption to project.py
    Encryption now runs, just need to see if we're doing it correctly and need to get decryption to correctly reverse it.

--- project.py
+++ project.py
@@ -46,15 +46,18 @@ def ecdhKeyExchange():
 
     # print(compress(aliceSharedKey).encode('utf-8'))
 
-    print("Session Key in Base64 Encoding: \n", b64SessionKey)
+    # print("Session Key in Base64 Encoding: \n", b64SessionKey)
 
     # print(b64SessionKey.len())
-
+    
     sesKeyTransform = compress(aliceSharedKey)
+    # print(len(sesKeyTransform))
     sesKeyTransform = sesKeyTransform[2:]
-    sesKeyTransform = sesKeyTransform[:-1]
-    print("Transformed Key: ", sesKeyTransform)
-
+    if(len(sesKeyTransform) % 2 == 1):
+        sesKeyTransform = sesKeyTransform[:-1]
+    
+    print("\nTransformed Key: ", sesKeyTransform, "\n")
+    
     return binascii.unhexlify(sesKeyTransform)
 
 def pkcs7padding(data, block_size=16):
@@ -70,27 +73,43 @@ def encrypt(pTextMsg, sesKey):
 
     iv = Random.new().read(AES.block_size)
     
-    print(iv)
+    # print(iv)
 
     byteString = pTextMsg.encode('utf-8')
 
-    paddedString = pkcs7padding(byteString)
+    paddedBString = pkcs7padding(byteString)
 
-    paddedMsg = paddedString.decode('utf-8')
+    # paddedMsg = paddedString.decode('utf-8')
 
-    print("Raw Text: ", pTextMsg)
-    print("Padded Text: ", paddedMsg)
+    print("Raw Text: ", pTextMsg, "\n")
+    print("Padded Text: ", paddedBString, "\n")
 
     cipher = AES.new(sesKey, AES.MODE_CBC, iv)
 
+    cText = cipher.encrypt(paddedBString)
+
     return {
-       'cipher_text': base64.b64encode(cipher.encrypt(paddedMsg)),
+       'cipher_text': base64.b64encode(cipher.encrypt(paddedBString)),
        'iv': base64.b64encode(iv)
     }
 
-def decrypt(cTextMsg, sesKey):
+def decrypt(encryption_dict, sesKey):
 
-    pTextMsg = cTextMsg
+    cText = base64.b64decode(encryption_dict['cipher_text'])
+    iv = base64.b64decode(encryption_dict['iv'])
+
+    cipher = AES.new(sesKey, AES.MODE_CBC, iv)
+
+    decryption = cipher.decrypt(cText)
+
+    pTextMsg = decryption.decode('utf-8')
+
+    pTextMsg = hex(pTextMsg)
+
+    # pTextMsg = decryption.decode('utf-8')
+
+    # pTextMsg = pTextMsg.rstrip()
+
     return pTextMsg
 
 def hashMsg(string):
@@ -104,9 +123,15 @@ pTextMsg = input("Enter a message: ")
 
 sessionKey = ecdhKeyExchange()
 
-print("Key used for encryption: ", sessionKey)
+# print("Key used for encryption: ", sessionKey, "\n")
 
-print(encrypt(pTextMsg, sessionKey))
+enc_dict = encrypt(pTextMsg, sessionKey)
+
+cipherText = base64.b64decode(enc_dict['cipher_text'])
+
+print("Cipher Text ==> ", cipherText, "\n")
+
+print("Decrypted Cipher ==> ", decrypt(enc_dict, sessionKey), "\n")
 
 # hashedPlaintext = hashMsg(pTextMsg)
 
